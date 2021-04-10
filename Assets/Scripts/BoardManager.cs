3b6d35e0248016b0d3081bd0384af041ba522b39
gnqo7598 <gnqo7598@gmail.com>
2021-04-11 04:04:04 +0900

    Initialization

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+
+    void BoardSetup()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
